81e45e450adbb795000e031575d1a4651bcc79cb
JacekJochemczyk <j.jochemczyk@gmail.com>
2025-01-05 17:43:49 +0100

    Dodaj pliki projektów.

---
+++ BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontoBankowe
+{
+    internal class BankAccount
+    {
+        private string _accountNumber;
+        private decimal _saldo;
+
+        public BankAccount(string accountNumber, decimal saldo)
+        {
+            _accountNumber = accountNumber;
+            _saldo = saldo;
+        }
+
+        public string GetAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public decimal GetSaldo()
+        {
+            return _saldo;
+        }
+
+        public void Wplata(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Kwota wpłaty musi być większa od zera.");
+
+            }
+
+            _saldo += amount;
+        }
+
+        public void Wyplata(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Kwota wyyplaty musi byc wieksza od zera");
+            }
+
+            if (amount > _saldo)
+            {
+                throw new InvalidOperationException("Niewystarczajace srodki na koncie.");
+
+            }
+            _saldo -= amount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace KontoBankowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("123456789123", 2000);
+
+            Console.WriteLine("Numer konta: " + account.GetAccountNumber());
+            Console.WriteLine("Saldo: " + account.GetSaldo() + " zł");
+
+            account.Wplata(500m);
+            Console.WriteLine("Saldo po wpłacie: " + account.GetSaldo() + " zł");
+
+            account.Wyplata(300m);
+            Console.WriteLine("Saldo po wypłacie: " + account.GetSaldo() + " zł");
+
+            try
+            {
+                account.Wyplata(3000m);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Błąd: " + ex.Message);
+            }
+        }
+
+    }
+}
